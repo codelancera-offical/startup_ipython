b6eeddf7401b777d0f741bb0ead68ee0d9d9a020
codelancera-phone <lancera.thn@gmail.com>
2024-09-05 15:49:48 -0700

    done 1.0

--- 00-startup.py
+++ 00-startup.py
@@ -1,5 +1,13 @@
-import sys
-# sys.path.append("/path/to/my_tools")
+print(
+"""
+CodelancerA's ipython_startup config
+
+Packages auto imported:
+    - my_tools.Net_Tools
+    - my_tools.OS_Tools
+"""
+)
+
+# 1.0 
 from my_tools.Net_Tools import *
 from my_tools.OS_Tools import *
-print("Packages imported: my_tools.Net_Tools, my_tools.OS_Tools")
