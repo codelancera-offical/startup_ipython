907714588323e959d51faa2b3fbdb2391f85fc37
CodelancerA <lancera.thn@gmail.com>
2024-09-05 15:43:25 -0700

    init ipython startup package

---
+++ 00-startup.py
@@ -0,0 +1,5 @@
+import sys
+# sys.path.append("/path/to/my_tools")
+from my_tools.Net_Tools import *
+from my_tools.OS_Tools import *
+print("Packages imported: my_tools.Net_Tools, my_tools.OS_Tools")
